42d31ceecf88d31537fa1aa79f13703f86eed847
Carlos Damian Chavez Gonalez <carlos_dami12@live.com>
2021-12-07 20:16:01 -0600

    migraciones listas

---
+++ database/migrations/2021_12_08_020454_create_data_clients_controllers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataClientsControllersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_clients_controllers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('companyName');
+            $table->text('businessTurn');
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_clients_controllers');
+    }
+}
