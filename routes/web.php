ef3c2f705b869be482a72940d66bda7082ee4ad2
Carlos Damian Chavez Gonalez <carlos_dami12@live.com>
2021-12-07 20:45:43 -0600

    modelo para traer los datos de los clientes

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/dataClients', [\App\Http\Controllers\DataClientController::class, 'index'])->middleware('auth');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
